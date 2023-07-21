8bfb3bf73dd307b88ce6a190cfb2ed5a9f921de9
Richy <richy@webcomm.com.tw>
2023-07-21 16:04:59 +0800

    new .swift file

---
+++ iOS_HelloNewSet/NewViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewViewController.swift
+//  iOS_HelloNewSet
+//
+//  Created by 王麒翔 on 2023/7/21.
+//
+
+import Foundation
+import UIKit
+
+class NewViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
